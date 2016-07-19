d80372d0c7eb37d76ef0a209a9fe7d86773c22d1
Jackson Kearl <jkearl@mit.edu>
2016-07-18 21:49:29 -0400

    Cacheing?

--- Server.js
+++ Server.js
@@ -8,7 +8,7 @@ http.createServer(function(request, response) {
 
   var uri = url.parse(request.url).pathname
     , filename = path.join(process.cwd(), uri);
-  
+
   path.exists(filename, function(exists) {
     if(!exists) {
       response.writeHead(404, {"Content-Type": "text/plain"});
@@ -20,12 +20,13 @@ http.createServer(function(request, response) {
     if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
     fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
+      if(err) {
         response.writeHead(500, {"Content-Type": "text/plain"});
         response.write(err + "\n");
         response.end();
         return;
       }
+      if (!response.getHeader('Cache-Control')) response.setHeader('Cache-Control', 'public, max-age=' + (maxAge / 1000));
 
       response.writeHead(200);
       response.write(file, "binary");
